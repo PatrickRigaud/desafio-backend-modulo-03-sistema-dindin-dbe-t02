30d233e5fa5b95f445a7dad0813bf536530876ad
Luana Ruver <luana_ruver@hotmail.com>
2023-08-13 00:12:32 -0300

    feat (rotas) - Criado novas rotas

--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const rotas = express();
-const {buscarUsuarios} = require('../controladores/controladores')
+const verificarUsuarioLogado = require('../controladores/intermediarios');
+const listarCategorias = require('../data/categoriasData');
+const { cadastrarUsuario, buscarUsuariosQuery, login } = require('../data/usuariosData');
 
+rotas.post('/usuario', cadastrarUsuario)
+rotas.post('/login', login)
 
+rotas.use(verificarUsuarioLogado)
 
-rotas.get('/usuario', buscarUsuarios)
+// rotas.get('/usuario', buscarUsuariosQuery)
+rotas.get('/:id', listarCategorias)
 
 
 module.exports = rotas;
