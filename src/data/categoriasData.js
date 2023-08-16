70c3a5e5f75b591e54c6fb792b6ac076b59d1312
Luana Ruver <luana_ruver@hotmail.com>
2023-08-16 19:22:58 -0300

    feat (data) - query categorias criadas

--- src/data/categoriasData.js
+++ src/data/categoriasData.js
@@ -1,20 +1,16 @@
-const db = require('../infra/database');
+const db = require("../infra/database")
 
-const listarCategorias = async (req, res) => {
-    const {id} = req.params;
-    try {
-        const query = 'select * from categorias where usuario_id = $1';
-        const params = [id]
-        const resultado = await db.query(query, params);
-        if (resultado.rowCount === 0) {
-            return res.status(404).json({mensagem: 'Nenhuma categoria encontrada'});
-        } else {
-            return res.status(200).json(resultado.rows);
-        }
-    } catch (error) {
-        console.log(error.message);
-    }
+
+const listarCategoriasQuery = (id) => {
+    return db.query('select * from categorias where usuario_id = $1', [id]);
 };
 
+const detalharCategoriaQuery = (id, usuario_id) => {
+    return db.query('select * from categorias where id = $1 and usuario_id = $2', [id, usuario_id]);
+}
 
-module.exports = { listarCategorias }
+const cadastrarCategoriasQuery = (id, descricao) => {
+    return db.query('insert into categorias (usuario_id, descricao) values ($1, $2) returning *', [id, descricao]);
+}
+
+module.exports = { listarCategoriasQuery, detalharCategoriaQuery, cadastrarCategoriasQuery };
