6549db184b5c01f1eb18e8015cea7d0aac30f389
Luana Ruver <luana_ruver@hotmail.com>
2023-08-16 21:25:28 -0300

    feat (data) - query atualizar e excluir categorias criadas

--- src/data/categoriasData.js
+++ src/data/categoriasData.js
@@ -13,4 +13,17 @@ const cadastrarCategoriasQuery = (id, descricao) => {
     return db.query('insert into categorias (usuario_id, descricao) values ($1, $2) returning *', [id, descricao]);
 }
 
-module.exports = { listarCategoriasQuery, detalharCategoriaQuery, cadastrarCategoriasQuery };
+
+const atualizarCategoriaQuery = (alteracao, id) => {
+    return db.query('update categorias set descricao = $1 where id = $2', [alteracao, id]);
+}
+
+const verificarTransacoesPorCategoria = (id) => {
+    return db.query('select * from transacoes where categoria_id = $1', [id])
+}
+
+const excluirCategoriaQuery = (id) => {
+    return db.query('delete from categorias where id = $1', [id]);
+}
+
+module.exports = { listarCategoriasQuery, detalharCategoriaQuery, cadastrarCategoriasQuery, atualizarCategoriaQuery , verificarTransacoesPorCategoria , excluirCategoriaQuery };
